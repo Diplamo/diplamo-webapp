37aec537c193a6c70225b72f61c82196b654778d
Pablo <pablo@varelacastelo.com>
2021-11-17 20:40:22 +0100

    view page - restart from different template

--- src/pages/view-certificates.vue
+++ src/pages/view-certificates.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
-import { useHead } from '@vueuse/head'
+/**
+ * This is a Vue Component that will be
+ * automatically mapped to a entry on vue-router.
+ *
+ * You will be able to access this page at http://localhost:3000/navbar-blank-page-7
+ * Page uri will match related path to src/pages folder
+ *
+ * Read more about routing:
+ * @see /vite.config.ts
+ * @see /src/router.ts
+ */
 
+import { useHead } from '@vueuse/head'
 import { pageTitle } from '/@src/state/navbarLayoutState'
 
 pageTitle.value = 'View Certificates'
 useHead({
-  title: 'Profile - Ampersand',
+  title: 'View Certificates',
 })
 </script>
 
 <template>
-  <div class="page-content-inner">
-    <CraftedViewCertificates />
-  </div>
+  <CustomNavbarSearchLayout theme="center">
+    <!-- Content Wrapper -->
+    <div class="page-content-inner">
+      <!--
+        Page content goes here
+        You can see pages content samples from 
+        files in /src/components/pages directory
+      -->
+    </div>
+  </CustomNavbarSearchLayout>
 </template>
